bfa2fe1865bcfb59ee5860fea3aecde96a2ccddf
Mounika <mounika.tunga9@gmail.com>
2017-07-06 14:30:43 -0700

    confirm

---
+++ Node/CarsandCats/app.js
@@ -0,0 +1,45 @@
+var http = require('http');
+// fs module allows us to read and write content for responses!!
+var fs = require('fs');
+// creating a server using http module:
+var server = http.createServer(function (request, response){
+    // see what URL the clients are requesting:
+    console.log('client request URL: ', request.url);
+    // this is how we do routing:
+   if(request.url === '/') {
+        fs.readFile('index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if (request.url === "/cars") {
+         fs.readFile('./images/car1.jpg', 'utf8', function (errors, contents){
+             response.writeHead(200, {'Content-type': 'image/jpg'});
+             response.write(contents); 
+             response.end();
+         });
+    }
+    else if (request.url === "/cats") {
+         fs.readFile('./images/cat1.jpg', 'utf8', function (errors, contents){
+             response.writeHead(200, {'Content-type': 'image/jpg'});
+             response.write(contents); 
+             response.end();
+         });
+    }
+    else if (request.url === "/cats/new") {
+         fs.readFile('form.html', 'utf8', function (errors, contents){
+             response.writeHead(200, {'Content-type': 'text/html'});
+             response.write(contents); 
+             response.end();
+         });
+    }
+    // request didn't match anything:
+    else {
+        response.end('File not found!!!');
+    }
+});
+// tell your server which port to run on
+server.listen(8000);
+// print to terminal window
+console.log("Running in localhost at port 8000");
