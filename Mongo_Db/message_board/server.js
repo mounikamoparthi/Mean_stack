92e62c12535f7b1d1b7291d7e1a013a6eee16dd0
Mounika <mounika.tunga9@gmail.com>
2017-07-11 17:05:27 -0700

    confirm

--- Mongo_Db/message_board/server.js
+++ Mongo_Db/message_board/server.js
@@ -15,39 +15,79 @@ mongoose.Promise = global.Promise;
 var Schema = mongoose.Schema;
 // define Post Schema
 var PostSchema = new mongoose.Schema({
- text: {type: String, required: true }, 
+ name: {type:String, required: true, minlength: 4},
+ messagetext: {type: String, required: true }, 
  comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}]
 }, {timestamps: true });
 // define Comment Schema
 var CommentSchema = new mongoose.Schema({
+  name: {type:String, required: true, minlength: 4},
  _post: {type: Schema.Types.ObjectId, ref: 'Post'},
- text: {type: String, required: true }
+ commenttext: {type: String, required: true }
 }, {timestamps: true });
 // set our models by passing them their respective Schemas
 mongoose.model('Post', PostSchema);
 mongoose.model('Comment', CommentSchema);
 // store our models in variables
-var Post = mongoose.model('Post');
+var Post = mongoose.model('Post'); // var name is used in Post,find
 var Comment = mongoose.model('Comment');
-// route for getting a particular post and comments
-app.get('/posts/:id', function (req, res){
- Post.findOne({_id: req.params.id})
- .populate('comments')
- .exec(function(err, post) {
-      res.render('post', {post: post});
-        });
+
+// route for getting a particular post and comment
+app.get('/', function(req,res) {
+    Post.find({}).populate('comments').exec(function(err,x){
+        console.log('postmsg get',x )
+        res.render('index', {postmsg:x, errors:err});
+ })
+   
 });
+
+
 // route for creating one comment with the parent post id
-app.post('/posts/:id', function (req, res){
-  Post.findOne({_id: req.params.id}, function(err, post){
-         var comment = new Comment(req.body);
-         comment._post = post._id;
-         post.comments.push(comment);
-         comment.save(function(err){
-                 post.save(function(err){
-                       if(err) { console.log('Error'); } 
-                       else { res.redirect('/'); }
+app.post('/message', function (req, res){
+    console.log("POST DATA", req.body);
+  var postmsg = new Post({name: req.body.name, messagetext: req.body.message});
+         postmsg.save(function(err){
+                       if(err) { 
+                           console.log('Error in posting'); 
+                           errors = err;
+                           res.render('index', {errors: postmsg.errors});
+                        } 
+                       else {
+                           console.log("successfully added a post"); 
+                           res.redirect('/');
+                         }
                  });
          });
-   });
- });
+
+app.post('/comment', function (req, res){
+    Post.findOne({_id: req.body.msgid}, function(err, x){
+         console.log("POST DATA", req.body);
+            var postcmt = new Comment({name: req.body.name, commenttext: req.body.comment});
+            postcmt._post = x._id;
+            //console.log("entered")
+            x.comments.push(postcmt);
+            postcmt.save(function(err){
+                        if(err) { 
+                            console.log('Error in postcmt'); 
+                            errors = err;
+                            } 
+                        else {
+                            errors =""
+                            x.save(function(err){
+                                if(err){
+                                    console.log('errors',err);
+                                }
+                                else{
+                                    console.log("successfully added comment"); 
+                                    res.redirect('/');
+                                }
+                            })
+                            
+                            }
+                    });
+            });
+});
+  
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+});
--- Mongo_Db/mongoose_dashboard/server.js
+++ Mongo_Db/mongoose_dashboard/server.js
@@ -11,16 +11,18 @@ app.use(session({secret: 'codingdojo'}));
 app.use(express.static(path.join(__dirname, './static')));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
-mongoose.connect('mongodb://localhost/basic_mongoose');
+mongoose.connect('mongodb://localhost/mongoose_dashboard');
 mongoose.Promise = global.Promise;
 var MongooseSchema = new mongoose.Schema({
- name: {type: String, required: true, minlength: 6},
+ name: {type: String, required: true, minlength: 3},
+ color:{type: String, required: true, minlength: 3},
+ type: {type: String, required: true, minlength: 3}
 }, {timestamps: true})
-// mongoose.model('Quote', QuoteSchema);
-var Mongoose = mongoose.model('Mongoose',MongooseSchema);
+mongoose.model('Bird',MongooseSchema);
+var Mongoose = mongoose.model('Bird');
 
 app.get('/', function(req,res) {
-   Mongoose.find({}, function(error, results){
+   Bird.find({}, function(error, results){
         if(error){
             res.render('index', {title: 'you have errors!', errors:error})
         }
@@ -30,6 +32,25 @@ app.get('/', function(req,res) {
     });
 })
 
-app.get('/mongooses/new', function(request,response){
-	response.render('newMongoose');
+app.get('/addnew', function(request,response){
+	response.render('addnew');
 })
+
+app.post('/birds', function(req,res){
+    console.log("POST DATA", req.body);
+    var bird_data = new Bird({name: req.body.name, color: req.body.color, type: req.body.type}) 
+    bird_data.save(function(err){
+        if(err){
+            console.log("Error in fetching bird_data");
+            errors = err;
+            res.render('addnew', {errors: bird_data.errors});
+        }
+        else{
+            console.log("Successfully added a bird");
+            res.redirect('/');
+        }
+    })
+})
+
+
+
